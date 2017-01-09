b3f0c767cd104583826daef172027bfc31b19450
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2017-01-09 14:54:05 -0600

    post route complete

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,1 +1,29 @@
 console.log('sourced');
+
+var myApp = angular.module( 'myApp', [] );
+
+myApp.controller('HeroController', ['$scope', '$http', function($scope, $http) {
+  console.log('hero controller');
+
+  $scope.addHero = function() {
+    console.log('in addHero');
+    //addemble heroToSend
+    var heroToSend = {
+      alias: $scope.aliasIn,
+      first_name: $scope.firstNameIn,
+      last_name: $scope.lastNameIn,
+      city: $scope.cityIn,
+      power_name: $scope.powerIn
+    }; // end heroToSend
+    console.log(heroToSend);
+    //send object to the server
+    $http({
+      method: 'POST',
+      url: '/hero',
+      data: heroToSend
+    }).then( function(response) {
+      console.log(response);
+    }); // end $http
+  }; // end addHero
+
+}]); // end HeroController
--- routes/hero.js
+++ routes/hero.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var Hero = require('../models/heroModel.js');
 
 // get all heros
 router.get('/', function(req, res) {
@@ -8,7 +9,10 @@ router.get('/', function(req, res) {
 
 // post to create a new hero
 router.post('/', function(req, res) {
-
+  console.log('post route hit', req.body);
+  var newHero = Hero(req.body);
+  newHero.save();
+  res.sendStatus(200);
 });
 
 module.exports = router;
